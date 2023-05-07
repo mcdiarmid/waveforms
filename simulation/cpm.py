10528531bdef87ff6476229bc2dd52e6717da714
mcdiarmid <campbell.mcdiarmid@icloud.com>
2023-05-08 10:54:09 +1200

    Added eye diagram, and trellis view for trajectory.

--- simulation/cpm.py
+++ simulation/cpm.py
@@ -1,10 +1,13 @@
+import random
+
 import numpy as np
 from numpy.typing import NDArray
 import matplotlib.pyplot as plt
 
 
-DATA_BUFFER = b"\x1b\x1bHello!"
-
+DATA_HEADER = b"\x1b\x1bHello World!"
+DATA_EXTRA = bytes([random.randint(0,0xff) for i in range(40)])
+DATA_BUFFER = DATA_HEADER + DATA_EXTRA
 
 def freq_pulse_soqpsk_tg(
     T_1: float = 1.5,
@@ -59,7 +62,7 @@ if __name__ == "__main__":
     # Bits of information to transmit
     j = complex(0, 1)
     symbol_rate = 1e3
-    sps = 20
+    sps = 64
     mod_index = 1/2
     bit_array = np.unpackbits(np.frombuffer(DATA_BUFFER, dtype=np.uint8))
 
@@ -73,9 +76,9 @@ if __name__ == "__main__":
     interpolated_symbols[sps::sps] = symbols
 
     # Generate pulse filter
-    fig_eye, eye_ax = plt.subplots(1)
-    eye_ax.set_ylabel("Imaginary")
-    eye_ax.set_xlabel("Real")
+    fig_eye, eye_const_axes = plt.subplots(3,2)
+    # constellation_ax.set_ylabel("Imaginary")
+    # constellation_ax.set_xlabel("Real")
 
     # Set-up some plots
     fig_time, (symbol_ax, trajectory_ax, iq_ax) = plt.subplots(3, 1)
@@ -83,29 +86,38 @@ if __name__ == "__main__":
     symbol_ax.stem(range(symbols.size+2), [0, *symbols, 0], linefmt='ko')
 
     iq_ax.set_ylabel("Amplitude [V]")
-    trajectory_ax.set_xlabel("Symbol time [t/T]")
+    iq_ax.set_xlabel("Symbol time [t/T]")
     trajectory_ax.set_ylabel("Phase [rad]")
+    trajectory_ax.set_yticks([(2*i+1)*np.pi/4 for i in range(4)])
+    trajectory_ax.set_yticklabels([rf"$\frac{{{2*i+1}\pi}}{{4}}$" for i in range(4)])
 
     # Generate and plot SOQPSK-MIL and SOQPSK-TG modulated data
-    for pulse_filter_fn in (freq_pulse_soqpsk_mil, freq_pulse_soqpsk_tg):
+    pulse_filter_fns = freq_pulse_soqpsk_mil, freq_pulse_soqpsk_tg
+    for n, pulse_filter_fn in enumerate(pulse_filter_fns):
         pulse_filter = pulse_filter_fn(sps=sps)
         freq_pulses = np.convolve(interpolated_symbols, pulse_filter, mode="same")
-        phi = 2 * np.pi * mod_index * np.cumsum(freq_pulses) / sps
+        phi = 2 * np.pi * mod_index * np.cumsum(freq_pulses) / sps + np.pi/4
         modulated_signal = np.exp(j*phi)
 
         # Display things
         iq_ax.plot(t, modulated_signal.real, linestyle='-')
         iq_ax.plot(t, modulated_signal.imag, linestyle='-')
         symbol_ax.plot(t, freq_pulses, linestyle='-')
-        trajectory_ax.plot(t, phi % (2*np.pi), marker='o', markersize=2)
-        trajectory_ax.plot(t, phi, markersize=2)
-        eye_ax.plot(modulated_signal.real, modulated_signal.imag, marker='o', linestyle='--')
+        trajectory_ax.plot(t%8, phi % (2*np.pi), linestyle='', marker='.', markersize=0.5)
+        # trajectory_ax.plot(t, phi, markersize=2)
+        eye_const_axes[0, n].plot(modulated_signal.real, modulated_signal.imag, linestyle='--')
+        eye_const_axes[1, n].plot(t%4, modulated_signal.real, marker='.', linestyle='', markersize=0.5)
+        eye_const_axes[2, n].plot((t+0.5)%4, modulated_signal.imag, marker='.', linestyle='', markersize=0.5)
 
     # Show plots
     symbol_ax.grid(which="both", linestyle=":")
+    symbol_ax.set_xlim([0, len(DATA_HEADER)*8+1])
     iq_ax.grid(which="both", linestyle=":")
+    iq_ax.set_xlim([0, len(DATA_HEADER)*8+1])
     trajectory_ax.grid(which="both", linestyle=":")
     fig_time.show()
 
-    eye_ax.grid(which="both", linestyle=":")
+    for ax_row in eye_const_axes:
+        for ax in ax_row:
+            ax.grid(which="both", linestyle=":")
     fig_eye.show()
