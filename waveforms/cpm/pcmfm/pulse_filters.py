421c03825751c2d13fd42c4decaa5f2884caed12
mcdiarmid <campbell.mcdiarmid@icloud.com>
2023-05-13 21:16:25 +1200

    Fixed PCM-FM NRZ-Bessel by using scipy.signal.besselap

--- waveforms/cpm/pcmfm/pulse_filters.py
+++ waveforms/cpm/pcmfm/pulse_filters.py
@@ -1,13 +1,16 @@
 import numpy as np
 from numpy.typing import NDArray
+from scipy.signal import besselap, impulse
 
-from waveforms.cpm.pcmfm.bessel import bessel
 from waveforms.cpm.helpers import normalize_cpm_filter
 
 
 def freq_pulse_pcmfm(sps: int = 8, order: int = 4) -> NDArray[np.float64]:
-    g1 = np.ones(sps) / (sps)
-    g2 = bessel(sps, order=order)
-    # g2 = np.ones(1)
+    length = 3
+    g1 = np.ones(sps) / (2*sps)
+    _t, g2 = impulse(
+        besselap(order, norm="mag"),
+        T=np.linspace(0, length*2/0.7, num=(length-1)*sps+1)
+    )
     g = np.convolve(g1, g2, mode="full")
     return normalize_cpm_filter(sps, g)
