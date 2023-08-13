5786831326c9e1c43ed7db64edb808cd49468957
Campbell McDiarmid <campbell.mcdiarmid@icloud.com>
2023-08-13 23:06:05 +1200

    Removed some redundant stuff, move some class methods to helper functions.

--- waveforms/viterbi/trellis.py
+++ waveforms/viterbi/trellis.py
@@ -2,6 +2,7 @@ import logging
 from typing import List, Dict
 from dataclasses import dataclass
 
+
 _logger = logging.getLogger(__name__)
 
 
@@ -14,13 +15,32 @@ class Branch:
 
 
 @dataclass
-class State:
-    branches: List[Branch]
-
-
-@dataclass
 class Trellis:
     branches: List[Branch]
+
+
+def filter_branches(
+    state: int,
+    branches: List[Branch],
+    attr: str = "start"
+) -> List[Branch]:
+    return [*filter(lambda b: getattr(b, attr) == state, branches)]
+
+
+def forward_branches(state: int, branches: List[Branch]) -> List[Branch]:
+    return filter_branches(state, branches, attr="start")
+
+
+def reverse_branches(state: int, branches: List[Branch]) -> List[Branch]:
+    return filter_branches(state, branches, attr="end")
+
+
+def forward_map(state: int, branches: List[Branch]) -> Dict[int, int]:
+    return {branch.inp: branch.end for branch in filter_branches(state, branches, "start")}
+
+
+def reverse_map(state: int, branches: List[Branch]) -> Dict[int, int]:
+    return {branch.out: branch.start for branch in filter_branches(state, branches, "end")}
 
 
 class FiniteStateMachine:
@@ -29,31 +49,22 @@ class FiniteStateMachine:
         self.state = start
         # TODO check trellis validity
 
-    def forward_branches(self) -> List[Branch]:
-        return [*filter(lambda b: b.start == self.state, self.trellis.branches)]
-
-    def backward_branches(self) -> List[Branch]:
-        return [*filter(lambda b: b.end == self.state, self.trellis.branches)]
-
-    def forward_map(self) -> Dict[int, int]:
-        return {branch.inp: branch.end for branch in self.forward_branches()}
-
-    def backward_map(self) -> Dict[int, int]:
-        return {branch.out: branch.start for branch in self.backward_branches()}
+        # Generate forward and reverse map for quick access
+        states = max(trellis.branches, key=lambda b: b.start).start
+        self.forward_mapping = [forward_map(s, trellis.branches) for s in range(states+1)]
+        self.reverse_mapping = [reverse_map(s, trellis.branches) for s in range(states+1)]
 
     def next(self, inp: int):
-        mapping = self.forward_map()
-        if inp not in mapping:
+        if inp not in self.forward_mapping[self.state]:
             _logger.warning(f"No branch corresponding to input {inp} from starting state {self.state}")
             return
-        self.state = mapping[inp]
+        self.state = self.forward_mapping[self.state][inp]
 
     def prev(self, out: int):
-        mapping = self.backward_map()
-        if out not in mapping:
+        if out not in self.reverse_mapping[self.state]:
             _logger.warning(f"No branch corresponding to output {out} from ending state {self.state}")
             return
-        self.state = mapping[out]
+        self.state = self.reverse_mapping[self.state][out]
 
 
 SOQPSKTrellis = Trellis(
