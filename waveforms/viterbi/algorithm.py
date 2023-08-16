b28ac275e2d549406f7efa330ccc1d5e65e7d8e4
mcdiarmid <campbell.mcdiarmid@icloud.com>
2023-08-16 18:15:46 +1200

    VA: Rename unclear variable names and formatting.

--- waveforms/viterbi/algorithm.py
+++ waveforms/viterbi/algorithm.py
@@ -3,11 +3,7 @@ from typing import Tuple
 import numpy as np
 from numpy.typing import NDArray
 
-from waveforms.viterbi.trellis import (
-    FiniteStateMachine,
-    Trellis,
-    Branch,
-)
+from waveforms.viterbi.trellis import FiniteStateMachine
 
 
 def viterbi_algorithm(
@@ -24,40 +20,44 @@ def viterbi_algorithm(
     :return: Array of hard decisions for the given input increments and fsm
     """
     # Initialize arrays
-    O,N = increments.shape
-    S = fsm.states
+    _output_cardinality, n_transitions = increments.shape
+    n_states = fsm.states
     increments = np.abs(increments)
-    recovered_symbols = np.zeros(N)
-    output = np.zeros(N)
+    recovered_symbols = np.zeros(n_transitions)
+    output = np.zeros(n_transitions)
 
     # Standard Viterbi Algorithm
-    branch_metric = np.zeros((S, 2), dtype=np.float64)
-    path = np.zeros((S, N), dtype=np.uint8)
+    branch_metric = np.zeros((n_states, 2), dtype=np.float64)
+    path = np.zeros((n_states, n_transitions), dtype=np.uint8)
 
     # Assign initial state likelihood based on previous viterbi iteration
-    branch_metric[:,0] = start or np.zeros(S)
+    branch_metric[:, 0] = start or np.zeros(n_states)
 
-    for j in range(N):
+    # Calculate branch metrics and winning paths going forward
+    for j in range(n_transitions):
         # Iterate all viable states
-        for i in range(S):
+        for st in range(n_states):
             # Iterate branches to determine most likely prior state
             min_k = 0
             min_m = np.inf
-            for branch in fsm.reverse_branch_mapping[i].values():
-                mm = branch_metric[branch.start,j%2] + increments[fsm.symbols.index(branch.out)][j]
+
+            for branch in fsm.reverse_branch_mapping[st].values():
+                symbol_index = fsm.symbols.index(branch.out)
+                mm = branch_metric[branch.start, j % 2] + increments[symbol_index, j]
+
                 if mm < min_m:
                     min_m = mm
                     min_k = branch.start
 
-            branch_metric[i,(j+1)%2] = min_m
-            path[i,j] = min_k
+            branch_metric[st, (j + 1) % 2] = min_m
+            path[st, j] = min_k
 
-        branch_metric[:,(j+1)%2] -= np.min(branch_metric[:,(j+1)%2])
+        branch_metric[:, (j + 1) % 2] -= np.min(branch_metric[:, (j + 1) % 2])
 
-    # Traceback
-    state = np.argmin(branch_metric[:, N%2])
-    for j in reversed(range(N)):
-        branch = fsm.reverse_transitions[state][path[state,j]]
+    # Traceback along winning path
+    state = np.argmin(branch_metric[:, n_transitions % 2])
+    for j in reversed(range(n_transitions)):
+        branch = fsm.reverse_transitions[state][path[state, j]]
         output[j] = branch.inp
         recovered_symbols[j] = branch.out
         state = path[state, j]
